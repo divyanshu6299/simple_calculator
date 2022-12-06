793d974d7a55d4e44feda16616cf056f68a2d5c0
divyanshu6299 <118836002+divyanshu6299@users.noreply.github.com>
2022-12-06 20:48:03 +0530

    Add files via upload

---
+++ calcuuuu.java
@@ -0,0 +1,40 @@
+package simple_calculator;
+import java.util.*;
+public class calcuuuu {
+
+	public static void main(String[] args) {
+		char operator;
+	    Double number1, number2, result;
+	    Scanner d = new Scanner(System.in);
+	    System.out.println("Choose an operator: +, -, *, or /");
+	    operator = d.next().charAt(0);
+	    System.out.println("Enter first number");
+	    number1 = d.nextDouble();
+	    System.out.println("Enter second number");
+	    number2 = d.nextDouble();
+        switch (operator) {
+	      case '+':
+	        result = number1 + number2;
+	        System.out.println(number1 + " + " + number2 + " = " + result);
+	        break;
+	      case '-':
+	        result = number1 - number2;
+	        System.out.println(number1 + " - " + number2 + " = " + result);
+	        break;
+	      case '*':
+	        result = number1 * number2;
+	        System.out.println(number1 + " * " + number2 + " = " + result);
+	        break;
+	      case '/':
+	        result = number1 / number2;
+	        System.out.println(number1 + " / " + number2 + " = " + result);
+	        break;
+	      default:
+	        System.out.println("Invalid operator!");
+	        break;
+	    }
+
+	    d.close();
+	}
+}
+
